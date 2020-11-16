c492f16f3857e04be30b0be10e0fee7f9d6011cd
sshimabuku <shimasstrafez@gmail.com>
2020-11-16 09:14:39 -0500

    Implemented Sequences class structure

---
+++ LevelEvaluation/src/main/java/com/javawalkthrough/levelevaluation/Sequences.java
@@ -0,0 +1,129 @@
+package com.javawalkthrough.levelevaluation;
+
+public class Sequences
+{
+    /*
+    * ---------- DO NOT MODIFY THIS METHOD ----------
+    * This method calls all methods within the Sequences class
+    */
+    public static void main( String[] args )
+    {
+        System.out.println( "First 10 odd numbers:" );
+        printOddNumbers();
+
+        System.out.println();
+
+        System.out.println( "First 10 multiples of 2 and 3:" );
+        printMultiplesOf2And3();
+
+        System.out.println();
+
+        System.out.println( "Simple line:" );
+        printLine();
+
+        System.out.println();
+
+        System.out.println( "Complex line:" );
+        printLineComplex();
+
+        System.out.println();
+
+        System.out.println( "Default rectangle:" );
+        printRectangle();
+
+        System.out.println();
+
+        System.out.println( "4 x 4 rectangle:" );
+        printRectangleComplex(4);
+
+        System.out.println();
+
+        System.out.println( "10 x 10 rectangle:" );
+        printRectangleComplex(10);
+
+        System.out.println();
+
+        System.out.println( "Default triangle:" );
+        printTriangle();
+    }
+
+
+    /*
+    * This method prints the first 5 odd numbers
+    * Output:
+    * 1
+    * 3
+    * 5
+    * 7
+    * 9
+    */
+    public static void printOddNumbers() {
+
+    }
+
+    /*
+    * This method prints the first 5 multiples of both 2 and 3
+    * Output:
+    * 6
+    * 12
+    * 18
+    * 24
+    * 30
+    */
+    public static void printMultiplesOf2And3() {
+
+    }
+
+
+    /*
+    * This method prints a line of 10 * with a loop
+    * Output: **********
+    */
+    public static void printLine() {
+
+    }
+
+    /*
+     * This method prints a line with a length of 10
+     * by alternating * and - with a loop
+     * Output: *-*-*-*-*-
+     */
+    public static void printLineComplex() {
+
+    }
+
+    /*
+    * This method prints the following shape:
+    *           ***
+    *           * *
+    *           ***
+    */
+    public static void printRectangle() {
+
+    }
+
+    /*
+     * This method prints a shape with n lines and columns
+     * where n is the length parameter.
+     * Output for 5:
+     *           *****
+     *           *   *
+     *           *   *
+     *           *   *
+     *           *****
+     */
+    public static void printRectangleComplex(int length) {
+
+    }
+
+    /*
+    * This method prints a triangle
+    * Output:
+    *           *
+    *          ***
+    *         *****
+    */
+    public static void printTriangle() {
+
+    }
+}
